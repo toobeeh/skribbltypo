49c946ea9d85a0da2141f1f67cb236abd80263b4
sbarrack <12619603+sbarrack@users.noreply.github.com>
2021-03-04 15:39:20 -0800

    prevent keybinds when typing in chat

--- features/keybinds.js
+++ features/keybinds.js
@@ -1,8 +1,17 @@
 (() => {
+    const chatInput = document.querySelector('#inputChat');
+    
     document.addEventListener('keydown', e => {
         if (document.activeElement.tagName !== 'INPUT') {
             if (e.key === 'z' && e.ctrlKey) {
+                e.preventDefault();
                 captureCanvas.restoreDrawing(1);
+                return;
+            }
+            if (e.key === 'Shift' && !(e.altKey || e.ctrlKey)) {
+                e.preventDefault();
+                chatInput.focus();
+                return;
             }
         }
     });
