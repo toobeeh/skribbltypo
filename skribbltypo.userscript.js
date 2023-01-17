ac3817177bdaf93933ce79ca99e1ca82a12a066a
Continuous Integration <toobeeh@users.noreply.github.com>
2023-01-17 03:47:54 +0000

    Userscript Build Artifacts

--- skribbltypo.userscript.js
+++ skribbltypo.userscript.js
@@ -5,7 +5,7 @@
 // @author tobeh#7437
 // @description Userscript version of skribbltypo - the most advanced toolbox for skribbl.io
 // @icon64 https://rawcdn.githack.com/toobeeh/skribbltypo/d416e4f61888b48a9650e74cf716559904e2fcbf/res/icon/128MaxFit.png
-// @version 24.1.3.1673925607302
+// @version 24.1.3.1673927274915
 // @updateURL https://raw.githubusercontent.com/toobeeh/skribbltypo/master/skribbltypo.userscript.js
 // @grant none
 // @match https://skribbl.io/*
@@ -2434,129 +2434,137 @@ visuals.init(); //init visual options popup
 let currentNodes = document.getElementsByTagName("*");
 
 // inject patched game.js and modify elements that are immediately after page load visible
+let patchNode = async (node) => {
+    if (localStorage.visualOptions && (node.tagName == "BODY" || node.tagName == "IMG")) { // head or image is loaded
+        // load current options
+        let opts = JSON.parse(localStorage.visualOptions);
+        visuals.applyOptions(opts);
+        // check if theme querystring is active
+        let name = (new URLSearchParams(window.location.search)).get("themename");
+        let theme = JSON.parse((new URLSearchParams(window.location.search)).get("theme"));
+        if (name && theme) {
+            window.history.pushState({}, document.title, "/");
+            if (visuals.themes.some(t => JSON.stringify(t.options) == JSON.stringify(theme))){
+                visuals.applyOptions(theme);
+                localStorage.visualOptions = JSON.stringify(theme);
+                setTimeout(() => new Toast("🥳 Activated theme " + name), 200);
+            }
+            else {
+                visuals.addTheme(name, theme);
+                visuals.applyOptions(theme);
+                localStorage.visualOptions = JSON.stringify(theme);
+                setTimeout(() => new Toast("🥳 Imported theme " + name), 200);
+            }
+        }
+    }
+    if (node.tagName == "SCRIPT" && node.src.includes("game.js")) {
+        // block game.js
+        node.type = "javascript/blocked"; // block for chrome
+        node.addEventListener("beforescriptexecute", e => e.preventDefault(), { once: true }); // block for firefox
+        node.src = ""; /* to be sure */
+        // insert patched script
+        let script = document.createElement("script");
+        script.src = chrome.extension.getURL("gamePatch.js");
+        node.parentElement.appendChild(script);
+        // add var to get access typo ressources in css
+        document.head.appendChild(elemFromString(`<style>:root{--typobrush:url(${chrome.runtime.getURL("res/brush.gif")})}</style>`));
+        
+     }
+     if (node.classList && node.classList.contains("button-play")) {
+         node.insertAdjacentHTML("beforebegin", "<div id='typoUserInfo'><bounceload></bounceload> Connecting to Typo server...</div>");
+     }
+     if (node.parentElement?.classList.contains("panels") && node.tagName == "DIV" && node.classList.contains("panel") && !node.classList.contains("patched")) {
+         const panelGrid = elemFromString("<div id='panelgrid'></div>");
+         node.parentElement.insertBefore(panelGrid, node);
+         node.classList.add("patched");
+         const leftCard = elemFromString(`<div class='panel patched' > 
+            <div style="display:flex;height:100%;flex-direction:column;justify-content:space-between;" id="leftPanelContent">
+                <h2><span> Changelog</span><span>Typo News </span></h2>
+                <span>Hello there!</span><span>Enjoy the new skribbl update!<br> Check out the typo changelog; some features like typo pressure and size shortcuts have been added.</span>
+                <div class="panel" style="width:unset; border:none !important; font-size:0.8em;"><b>BTW, did you know?</b>
+                    <br>${hints[Math.floor(Math.random() * hints.length)]}
+                </div>
+                <div style="display: grid; grid-template-columns: 50% 50%;">
+                    <typosocial media="discord"><a target="_blank" href='https://typo.rip/discord'>Typo Discord</a></typosocial>
+                    <typosocial media="website"><a target="_blank"  href='https://typo.rip'>Typo Website</a></typosocial>
+                    <typosocial media="patreon"><a target="_blank"  href='https://patreon.com/skribbltypo'>Typo Patreon</a></typosocial>
+                    <typosocial media="github"><a target="_blank"  href='https://github.com/toobeeh/skribbltypo'>Typo GitHub</a></typosocial>
+                </div>
+            </div>
+            </div>`);
+         let popupChanges = elemFromString(changelogRawHTML);
+         leftCard.querySelector("h2 span").addEventListener("click", () => {
+             new Modal(popupChanges, () => { }, "Changelog");
+             localStorage.lastChangelogview = chrome.runtime.getManifest().version;
+         });
+
+         const rightCard = elemFromString(`<div class='panel patched' >
+            <div style="display:flex;height:100%;flex-direction:column;justify-content:space-between;" id="rightPanelContent" class="lobbies">
+                <h2><span>Sprite Cabin </span><span> Lobbies</span></h2>
+                <div id="lobbyBoard">
+                    <div id="discordLobbies"></div>
+                    <div id="lobbyFilters">
+                        <button id="addFilter" class="flatUI blue min air" style="margin: .5em">Add Filter</button>
+                    </div>
+                </div>
+                <div id="cabinSlots" class="unauth">
+                    <div id="loginRedir"><button class="flatUI air min blue">Log in with Palantir</button></div>
+                    <div>Slot 1<p></p></div>
+                    <div>Slot 2<p></p></div>
+                    <div>Slot 3<p></p></div>
+                    <div>Slot 4<p></p></div>
+                    <div>Slot 5<p></p></div>
+                    <div>Slot 6<p></p></div>
+                    <div>Slot 7<p></p></div>
+                    <div>Slot 8<p></p></div>
+                    <div>Slot 9<p></p></div>
+                </div>
+            </div>
+            </div>`);
+         panelGrid.appendChild(leftCard);
+         panelGrid.appendChild(node);
+         panelGrid.appendChild(rightCard);
+         QS("#rightPanelContent #loginRedir").addEventListener("click", login);
+         QS("#rightPanelContent h2").addEventListener("click", (event) => {
+             event.target.closest("#rightPanelContent").classList.toggle("cabin");
+             event.target.closest("#rightPanelContent").classList.toggle("lobbies");
+         });
+
+         // init socket
+        setTimeout(async () => {
+            lobbies.init();
+            await socket.init();
+        }, 0);
+     }
+}
 let patcher = new MutationObserver((mutations) => {
-         mutations.forEach((mutation) => {
-            let nodes = [...mutation.addedNodes];
-            nodes.push(...currentNodes);
-            currentNodes = [];
-             nodes.forEach(async function (node) {
-                if (localStorage.visualOptions && (node.tagName == "BODY" || node.tagName == "IMG")) { // head or image is loaded
-                    // load current options
-                    let opts = JSON.parse(localStorage.visualOptions);
-                    visuals.applyOptions(opts);
-                    // check if theme querystring is active
-                    let name = (new URLSearchParams(window.location.search)).get("themename");
-                    let theme = JSON.parse((new URLSearchParams(window.location.search)).get("theme"));
-                    if (name && theme) {
-                        window.history.pushState({}, document.title, "/");
-                        if (visuals.themes.some(t => JSON.stringify(t.options) == JSON.stringify(theme))){
-                            visuals.applyOptions(theme);
-                            localStorage.visualOptions = JSON.stringify(theme);
-                            setTimeout(() => new Toast("🥳 Activated theme " + name), 200);
-                        }
-                        else {
-                            visuals.addTheme(name, theme);
-                            visuals.applyOptions(theme);
-                            localStorage.visualOptions = JSON.stringify(theme);
-                            setTimeout(() => new Toast("🥳 Imported theme " + name), 200);
-                        }
-                    }
-                }
-                if (node.tagName == "SCRIPT" && node.src.includes("game.js")) {
-                    // block game.js
-                    node.type = "javascript/blocked"; // block for chrome
-                    node.addEventListener("beforescriptexecute", e => e.preventDefault(), { once: true }); // block for firefox
-                    node.src = ""; /* to be sure */
-                    // insert patched script
-                    let script = document.createElement("script");
-                    script.src = chrome.extension.getURL("gamePatch.js");
-                    node.parentElement.appendChild(script);
-                    // add var to get access typo ressources in css
-                    document.head.appendChild(elemFromString(`<style>:root{--typobrush:url(${chrome.runtime.getURL("res/brush.gif")})}</style>`));
-                    
-                 }
-                 if (node.classList && node.classList.contains("button-play")) {
-                     node.insertAdjacentHTML("beforebegin", "<div id='typoUserInfo'><bounceload></bounceload> Connecting to Typo server...</div>");
-                 }
-                 if (node.parentElement?.classList.contains("panels") && node.tagName == "DIV" && node.classList.contains("panel") && !node.classList.contains("patched")) {
-                     const panelGrid = elemFromString("<div id='panelgrid'></div>");
-                     node.parentElement.insertBefore(panelGrid, node);
-                     node.classList.add("patched");
-                     const leftCard = elemFromString(`<div class='panel patched' > 
-                        <div style="display:flex;height:100%;flex-direction:column;justify-content:space-between;" id="leftPanelContent">
-                            <h2><span> Changelog</span><span>Typo News </span></h2>
-                            <span>Hello there!</span><span>Enjoy the new skribbl update!<br> Check out the typo changelog; some features like typo pressure and size shortcuts have been added.</span>
-                            <div class="panel" style="width:unset; border:none !important; font-size:0.8em;"><b>BTW, did you know?</b>
-                                <br>${hints[Math.floor(Math.random() * hints.length)]}
-                            </div>
-                            <div style="display: grid; grid-template-columns: 50% 50%;">
-                                <typosocial media="discord"><a target="_blank" href='https://typo.rip/discord'>Typo Discord</a></typosocial>
-                                <typosocial media="website"><a target="_blank"  href='https://typo.rip'>Typo Website</a></typosocial>
-                                <typosocial media="patreon"><a target="_blank"  href='https://patreon.com/skribbltypo'>Typo Patreon</a></typosocial>
-                                <typosocial media="github"><a target="_blank"  href='https://github.com/toobeeh/skribbltypo'>Typo GitHub</a></typosocial>
-                            </div>
-                        </div>
-                        </div>`);
-                     let popupChanges = elemFromString(changelogRawHTML);
-                     leftCard.querySelector("h2 span").addEventListener("click", () => {
-                         new Modal(popupChanges, () => { }, "Changelog");
-                         localStorage.lastChangelogview = chrome.runtime.getManifest().version;
-                     });
-
-                     const rightCard = elemFromString(`<div class='panel patched' >
-                        <div style="display:flex;height:100%;flex-direction:column;justify-content:space-between;" id="rightPanelContent" class="lobbies">
-                            <h2><span>Sprite Cabin </span><span> Lobbies</span></h2>
-                            <div id="lobbyBoard">
-                                <div id="discordLobbies"></div>
-                                <div id="lobbyFilters">
-                                    <button id="addFilter" class="flatUI blue min air" style="margin: .5em">Add Filter</button>
-                                </div>
-                            </div>
-                            <div id="cabinSlots" class="unauth">
-                                <div id="loginRedir"><button class="flatUI air min blue">Log in with Palantir</button></div>
-                                <div>Slot 1<p></p></div>
-                                <div>Slot 2<p></p></div>
-                                <div>Slot 3<p></p></div>
-                                <div>Slot 4<p></p></div>
-                                <div>Slot 5<p></p></div>
-                                <div>Slot 6<p></p></div>
-                                <div>Slot 7<p></p></div>
-                                <div>Slot 8<p></p></div>
-                                <div>Slot 9<p></p></div>
-                            </div>
-                        </div>
-                        </div>`);
-                     panelGrid.appendChild(leftCard);
-                     panelGrid.appendChild(node);
-                     panelGrid.appendChild(rightCard);
-                     QS("#rightPanelContent #loginRedir").addEventListener("click", login);
-                     QS("#rightPanelContent h2").addEventListener("click", (event) => {
-                         event.target.closest("#rightPanelContent").classList.toggle("cabin");
-                         event.target.closest("#rightPanelContent").classList.toggle("lobbies");
-                     });
-
-                     // init socket
-                    setTimeout(async () => {
-                        lobbies.init();
-                        await socket.init();
-                    }, 0);
-                 }
-            });
-        });
+    mutations.forEach((mutation) => {
+        let nodes = [...mutation.addedNodes];
+        nodes.push(...currentNodes);
+        currentNodes = [];
+        nodes.forEach(patchNode);
+    })
 });
 patcher.observe(document.documentElement, { attributes: false, childList: true, subtree: true });
 
 
 
 
-    /* trigger patcher manually */
-    document.body.appendChild(document.createElement("div"));
+    /* disconnect patcher, not used */
+    patcher.disconnect();
 
+    /* get new document to re-run without original game js */
     let html = await (await fetch("./")).text();
     html = html.replaceAll("game.js", "game.jsx");
     const newDoc = document.createElement("html");
     newDoc.innerHTML = html;
     document.body = newDoc.querySelector("body");
+
+    /* patch nodes manually */
+    let nodes = document.querySelectorAll("*");
+    for(const node of nodes){
+        await patchNode(node);
+    }
 
     /* bundle styles */
     document.body.insertAdjacentHTML("afterbegin", `<style>﻿#game-chat{
@@ -3740,7 +3748,8 @@ bounceload {
 
 </style>`);
 
-
+    /* dispatch fake load events */
+    window.dispatchEvent(new Event("load"));
     document.dispatchEvent(new Event("DOMContentLoaded"));
 
     /* init popup polyfill */
