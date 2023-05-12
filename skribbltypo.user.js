f6b63192902090980debbcb83eb73cfdbee80d8b
Continuous Integration <toobeeh@users.noreply.github.com>
2023-05-12 13:39:18 +0000

    Userscript Build Artifacts

--- skribbltypo.user.js
+++ skribbltypo.user.js
@@ -5,7 +5,7 @@
 // @author tobeh#7437
 // @description Userscript version of skribbltypo - the most advanced toolbox for skribbl.io
 // @icon64 https://rawcdn.githack.com/toobeeh/skribbltypo/d416e4f61888b48a9650e74cf716559904e2fcbf/res/icon/128MaxFit.png
-// @version 24.3.5.4.168389000
+// @version 24.3.6.168389875
 // @updateURL https://raw.githubusercontent.com/toobeeh/skribbltypo/master/skribbltypo.userscript.js
 // @grant none
 // @match https://skribbl.io/*
@@ -24,7 +24,7 @@ const chrome = {
             return "https://rawcdn.githack.com/toobeeh/skribbltypo/d416e4f61888b48a9650e74cf716559904e2fcbf/" + url;
         },
         getManifest: () => {
-            return {version: "24.3.5.4 usrsc"};
+            return {version: "24.3.6 usrsc"};
         },
         onMessage: {
             addListener: (callback) => {
