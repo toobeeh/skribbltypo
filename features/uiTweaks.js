85a15984495402978843cf6f28995d9e90df0b97
tobeh <tobiasch@gmx.at>
2024-10-10 13:49:30 +0200

    fix lobby desc field size

--- features/uiTweaks.js
+++ features/uiTweaks.js
@@ -191,9 +191,9 @@ const uiTweaks = {
     initLobbyDescriptionForm: () => {
         // add Description form 
         let customwords = QS(".group-customwords, .game-room-group.customwords");
-        const input = elemFromString(`<div class="group-customwords game-room-group" style="height:10%">
+        const input = elemFromString(`<div class="group-customwords game-room-group" style="min-height:3rem">
 <div class="game-room-name">Palantir Description</div>
-<textarea style="" id="lobbyDesc" maxlength="200" spellcheck="false" placeholder="Add a description that will show up in the Palantir bot"></textarea>
+<textarea id="lobbyDesc" maxlength="200" spellcheck="false" placeholder="Add a description that will show up in the Palantir bot"></textarea>
 </div>`);
         customwords.insertAdjacentElement("beforebegin", input);
     },
