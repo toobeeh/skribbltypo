a92cf1dfa45c90fdf4dd03197c488d00098c87cb
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-01-24 05:17:04 +0100

    optimize pre-execution as fas as possible

--- src/content/pre-execution-block.ts
+++ src/content/pre-execution-block.ts
@@ -1,25 +1,4 @@
-/* hello there :) */
-console.log(`%c
-        _             _   _       _       _   _                           
-       | |           (_) | |     | |     | | | |     %cskribbl modded with%c
-  ___  | | __  _ __   _  | |__   | |__   | | | |_   _   _   _ __     ___  
- / __| | |/ / | '__| | | | '_ \\  | '_ \\  | | | __| | | | | | '_ \\   / _ \\ 
- \\__ \\ |   <  | |    | | | |_) | | |_) | | | | |_  | |_| | | |_) | | (_) |
- |___/ |_|\\_\\ |_|    |_| |_.__/  |_.__/  |_|  \\__|  \\__, | | .__/   \\___/ 
-                                                     __/ | | |            
-                                                    |___/  |_|     %cby tobeh#7437 %c
-
-        ➜ Typo & all its backend is open source: https://github.com/toobeeh/skribbltypo
-        ➜ Join the community: https://discord.com/invite/pAapmUmWAM
-        ➜ Find more infos at: https://www.typo.rip/
-        ➜ Support development: https://patreon.com/skribbltypo
-                                                                    
-                                                    `,
-  "color: lightblue",
-  "color:#2596be; font-family:'Arial'; font-weight:bold; font-style:italic; letter-spacing:2em",
-  "color: lightblue",
-  "color:#2596be; font-family:'Arial'; font-weight:bold; font-style:italic; letter-spacing:2em", "color:#f39656"
-);
+console.log("pre-execution-block.ts");
 
 /**
  * Intercept game.js script and block it from executing
@@ -42,7 +21,30 @@ const scriptObserver = new MutationObserver((mutations) => {
   });
 });
 
-scriptObserver.observe(document.body, {
+scriptObserver.observe(document, {
   childList: true,
   subtree: true
 });
+
+/* hello there :) */
+console.log(`%c
+        _             _   _       _       _   _                           
+       | |           (_) | |     | |     | | | |     %cskribbl modded with%c
+  ___  | | __  _ __   _  | |__   | |__   | | | |_   _   _   _ __     ___  
+ / __| | |/ / | '__| | | | '_ \\  | '_ \\  | | | __| | | | | | '_ \\   / _ \\ 
+ \\__ \\ |   <  | |    | | | |_) | | |_) | | | | |_  | |_| | | |_) | | (_) |
+ |___/ |_|\\_\\ |_|    |_| |_.__/  |_.__/  |_|  \\__|  \\__, | | .__/   \\___/ 
+                                                     __/ | | |            
+                                                    |___/  |_|     %cby tobeh#7437 %c
+
+        ➜ Typo & all its backend is open source: https://github.com/toobeeh/skribbltypo
+        ➜ Join the community: https://discord.com/invite/pAapmUmWAM
+        ➜ Find more infos at: https://www.typo.rip/
+        ➜ Support development: https://patreon.com/skribbltypo
+                                                                    
+                                                    `,
+  "color: lightblue",
+  "color:#2596be; font-family:'Arial'; font-weight:bold; font-style:italic; letter-spacing:2em",
+  "color: lightblue",
+  "color:#2596be; font-family:'Arial'; font-weight:bold; font-style:italic; letter-spacing:2em", "color:#f39656"
+);
