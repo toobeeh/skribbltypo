1fa5af1f1c961442e403c1f3a7a421fc5ef42041
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-03-19 12:49:11 +0100

    show new thumbs icons in message bubble toobeeh/skribbltypo#343

--- src/content/features/canvas-rate-icons/canvas-rate-icons.svelte
+++ src/content/features/canvas-rate-icons/canvas-rate-icons.svelte
@@ -6,12 +6,12 @@
         background-position: center;
     }
 
-  :global(#game-canvas #game-rate.typo-icons .like){
-    background-image: var(--file-img-thumbsup-gif);
+  :global(#game-canvas #game-rate.typo-icons .like, .player-bubble .icon[style*='thumbsup']){
+    background-image: var(--file-img-thumbsup-gif) !important;
   }
 
-  :global(#game-canvas #game-rate.typo-icons .dislike){
-      background-image: var(--file-img-thumbsdown-gif);
+  :global(#game-canvas #game-rate.typo-icons .dislike, .player-bubble .icon[style*='thumbsdown']){
+      background-image: var(--file-img-thumbsdown-gif) !important;
   }
 
 </style>
