ce8e345f9cb195e2b08957da791a959e2c3d5c95
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-03-26 18:23:39 +0100

    add firefox and userscript builds to nightly release toobeeh/skribbltypo#140 toobeeh/skribbltypo#139

--- vite-build/typo-build-extension.plugin.ts
+++ vite-build/typo-build-extension.plugin.ts
@@ -2,6 +2,7 @@ import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
 import ManifestV3 = chrome.runtime.ManifestV3;
 import manifest from "../src/runtime/extension/manifest";
 import { TypoBuildPlugin } from "./typo-build-plugin.interface";
+import ManifestV2 = chrome.runtime.ManifestV2;
 
 /**
  * Build typo as web extension for firefox or chrome
@@ -17,7 +18,13 @@ export const buildChromeExtension: (browser: "chrome" | "firefox") => TypoBuildP
   const mv3 = manifest as unknown as ManifestV3;
 
   /* add version name to manifest */
-  mv3.version_name = `${mv3.version} ${version}-crx${buildCommit ? " " + buildCommit : ""}`;
+  const browserShort = browser === "chrome" ? "cr" : "ff";
+  mv3.version_name = `${mv3.version} ${version}-${browserShort}x${buildCommit ? " " + buildCommit : ""}`;
+
+  /* change background worker declaration for firefox */
+  if(browser === "firefox") {
+    (mv3 as unknown as ManifestV2).background = {scripts: [mv3.background.service_worker]};
+  }
 
   const crxOriginal = crx({ manifest: mv3 as unknown as ManifestV3Export, browser });
 
