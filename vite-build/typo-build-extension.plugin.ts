722e705d309954ac016644087930e74b38e9af9e
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-04-28 22:42:55 +0200

    fix firefox workflow

--- vite-build/typo-build-extension.plugin.ts
+++ vite-build/typo-build-extension.plugin.ts
@@ -4,6 +4,15 @@ import manifest from "../src/runtime/extension/manifest";
 import { TypoBuildPlugin } from "./typo-build-plugin.interface";
 import ManifestV2 = chrome.runtime.ManifestV2;
 
+interface firefoxMv3 {
+  browser_specific_settings: {
+    gecko: {
+      id: string;
+      update_url: string;
+    }
+  }
+}
+
 /**
  * Build typo as web extension for firefox or chrome
  * based on the crx plugin
@@ -20,6 +29,17 @@ export const buildChromeExtension: (browser: "chrome" | "firefox") => TypoBuildP
   /* add version name to manifest */
   const browserShort = browser === "chrome" ? "cr" : "ff";
   mv3.version_name = `${mv3.version} ${version}-${browserShort}x${buildCommit ? " " + buildCommit : ""}`;
+
+  /* set update url for firefox */
+  if(browser === "firefox") {
+    mv3.update_url = "https://get.typo.rip/extension/firefox/updates.json";
+    (mv3 as unknown as firefoxMv3).browser_specific_settings = {
+      gecko: {
+        id: "{0b67dc2f-1517-451c-b5cb-8b28270b03e6}",
+        update_url: "https://get.typo.rip/firefox/updates.json"
+      }
+    };
+  }
 
   /* change background worker declaration for firefox */
   if(browser === "firefox") {
