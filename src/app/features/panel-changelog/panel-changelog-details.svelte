46aac69ded88d3709ed2e0d2e98fb9db1889fd15
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-05-04 13:21:11 +0200

    automatically open changelog notes on first load toobeeh/skribbltypo#369

--- src/app/features/panel-changelog/panel-changelog-details.svelte
+++ src/app/features/panel-changelog/panel-changelog-details.svelte
@@ -18,8 +18,20 @@
     }
 
   }
+
+  .info {
+    display: flex;
+    gap: 1.5rem;
+    justify-content: center;
+    font-size: .8em;
+    opacity: .8;
+  }
 </style>
 
+<div class="info">
+  <span>{new Date(Number(change.date)).toDateString()}</span>
+  <span>Update changelog for v{change.affectedTypoVersion}</span>
+</div>
 <br>
 <div class="details">
   <SvelteMarkdown source="{change.details}" />
