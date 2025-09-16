4822e6b527fa1526587fc96f8d03b7320c523e94
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-09-16 21:35:57 +0200

    add pink drop asset and check for league mode toobeeh/skribbltypo#404

--- src/app/features/drops/drops.feature.ts
+++ src/app/features/drops/drops.feature.ts
@@ -3,6 +3,7 @@ import { BooleanExtensionSetting } from "@/app/core/settings/setting";
 import { LobbyLeftEventListener } from "@/app/events/lobby-left.event";
 import { LobbyConnectionService } from "@/app/features/lobby-status/lobby-connection.service";
 import { ChatService } from "@/app/services/chat/chat.service";
+import { LobbyService } from "@/app/services/lobby/lobby.service";
 import { ToastService } from "@/app/services/toast/toast.service";
 import { ApiDataSetup } from "@/app/setups/api-data/api-data.setup";
 import type { DropAnnouncementDto, DropClaimResultDto } from "@/signalr/tobeh.Avallone.Server.Classes.Dto";
@@ -32,6 +33,7 @@ export class DropsFeature extends TypoFeature {
   @inject(ChatService) private readonly _chatService!: ChatService;
   @inject(ToastService) private readonly _toastService!: ToastService;
   @inject(LobbyLeftEventListener) private readonly _lobbyLeftEventListener!: LobbyLeftEventListener;
+  @inject(LobbyService) private readonly _lobbyService!: LobbyService;
 
   public readonly name = "Drops";
   public readonly description = "Show drops to collect extra bubbles when you're playing";
@@ -47,7 +49,7 @@ export class DropsFeature extends TypoFeature {
 
   private _component?: DropsComponent;
   private _recordedClaims$ = new BehaviorSubject<(DropClaimResultDto & {own: boolean})[]>([]);
-  private _currentDrop$ = new Subject<{ drop: DropAnnouncementDto, timestamp: number, ownClaimed: boolean } | undefined>();
+  private _currentDrop$ = new Subject<{ drop: DropAnnouncementDto, timestamp: number, ownClaimed: boolean, leagueMode: boolean } | undefined>();
   private _dropSummarySubscription?: Subscription;
   private _dropAnnouncedSubscription?: Subscription;
 
@@ -108,11 +110,17 @@ export class DropsFeature extends TypoFeature {
           ),
         ),
         tap((drop) => this._logger.info("Setting drop", drop)),
+        withLatestFrom(this._lobbyService.lobby$)
       )
-      .subscribe((drop) =>
+      .subscribe(([drop, lobby]) =>
         this._currentDrop$.next(
-          drop === undefined ? undefined : { drop, timestamp: Date.now(), ownClaimed: false },
-        ),
+          drop === undefined ? undefined : {
+            drop,
+            timestamp: Date.now(),
+            ownClaimed: false,
+            leagueMode: (lobby?.players.length === 1 || lobby?.players.every(player => player.score === 0)) ?? false
+          },
+        )
       );
 
     /* subscribe to changes in the current drop and build a summary of claims when it's cleared */
@@ -179,7 +187,7 @@ export class DropsFeature extends TypoFeature {
   public async claimDrop(drop: DropAnnouncementDto, timestamp: number) {
     this._logger.info("Claiming drop after delay", Date.now() - timestamp);
 
-    this._currentDrop$.next({drop, ownClaimed: true, timestamp}); /* claiming phase not finished, but player cannot claim anymore */
+    this._currentDrop$.next({drop, ownClaimed: true, timestamp, leagueMode: false}); /* claiming phase not finished, but player cannot claim anymore */
 
     try {
       const result = await this._lobbyConnectionService.connection.hub.claimDrop({
