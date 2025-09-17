0a520fc429ae978404811e9ff34cd155b6d9cdab
afx8732 <arcticfox8732@gmail.com>
2025-09-16 20:57:02 -0500

    show suggestion popover

--- src/app/features/ping-and-replies/ping-and-replies.feature.ts
+++ src/app/features/ping-and-replies/ping-and-replies.feature.ts
@@ -1,11 +1,14 @@
 import { FeatureTag } from "@/app/core/feature/feature-tags";
-import { BooleanExtensionSetting } from "@/app/core/settings/setting";
 import { ChatService } from "@/app/services/chat/chat.service";
 import { LobbyService } from "@/app/services/lobby/lobby.service";
+import type { componentData } from "@/app/services/modal/modal.service";
 import { ElementsSetup } from "@/app/setups/elements/elements.setup";
+import AreaFlyout from "@/lib/area-flyout/area-flyout.svelte";
+import { fromObservable } from "@/util/store/fromObservable";
 import { inject } from "inversify";
 import { BehaviorSubject, Subscription, withLatestFrom } from "rxjs";
 import { TypoFeature } from "../../core/feature/feature";
+import SuggestionPopover from "./suggestion-popover.svelte";
 
 export class PingAndRepliesFeature extends TypoFeature {
   @inject(ElementsSetup) private readonly _elements!: ElementsSetup;
@@ -18,30 +21,30 @@ export class PingAndRepliesFeature extends TypoFeature {
   public readonly tags = [FeatureTag.INTERFACE, FeatureTag.SOCIAL];
   public readonly featureId = 52;
 
-  private readonly _nameCompletionSetting = this.useSetting(
-    new BooleanExtensionSetting("ping_and_replies_complete", true, this),
-  )
-    .withName("Enable suggestions")
-    .withDescription("Shows an autocomplete window above the text box when pinging people.");
-
   private input: HTMLInputElement | undefined;
 
   private chatSubscription?: Subscription;
 
-  private _playerCandidates$ = new BehaviorSubject<string[] | null>([]);
+  // TODO: currently set on chat message, find api for that
+  private currentLobby = ["loading"];
+  private _playerCandidates$ = new BehaviorSubject<string[]>([]);
+  private _flyoutComponent?: AreaFlyout = undefined;
+  private _flyoutSubscription?: Subscription;
 
   protected override async onActivate() {
-    this._playerCandidates$.next(null);
+    this._playerCandidates$.next([]);
     const elements = await this._elements.complete();
     this.input = elements.chatInput;
     this.chatSubscription = this._chatSvc.playerMessageReceived$
       .pipe(withLatestFrom(this._lobbySvc.lobby$))
       .subscribe(([msg, lobby]) => {
         if (!lobby) return;
+        this.currentLobby = lobby.players.map((person) => person.name);
         const myName = lobby.players.find((x) => x.id == lobby?.meId);
         if (!myName) return;
         this.onMessage(msg.contentElement, msg.content, myName.name);
       });
+    this.input.addEventListener("keyup", () => this.onChatInput());
   }
 
   protected override async onDestroy() {
@@ -53,5 +56,72 @@ export class PingAndRepliesFeature extends TypoFeature {
     // adding a space for pings at end of message
     if (!(content + " ").includes(lookFor)) return;
     element.parentElement?.classList.add("guessed");
+  }
+
+  private onChatInput() {
+    const v = this.input?.value;
+    if (v === undefined) return;
+
+    const toComplete = v.split("@").at(-1);
+    if (!toComplete) {
+      this._flyoutComponent?.close();
+      this._flyoutComponent = undefined;
+      return;
+    }
+
+    const matches = this.currentLobby.filter((person) => person.startsWith(toComplete));
+    if (matches.length == 0) {
+      this._flyoutComponent?.close();
+      this._flyoutComponent = undefined;
+      return;
+    }
+
+    console.log(matches);
+    this._playerCandidates$.next(matches);
+    this.showAutocomplete();
+  }
+
+  private async showAutocomplete() {
+    if (this._flyoutComponent !== undefined) return;
+
+    const elements = await this._elements.complete();
+
+    /* create fly out content */
+    const flyoutContent: componentData<SuggestionPopover> = {
+      componentType: SuggestionPopover,
+      props: {
+        feature: this,
+        onSelected: (name: string) => {
+          console.log("selected", name);
+          this._flyoutComponent?.close();
+          console.log("do focus");
+        },
+      },
+    };
+
+    /* open flyout and destroy when clicked out */
+    this._flyoutComponent = new AreaFlyout({
+      target: elements.gameWrapper,
+      props: {
+        componentData: flyoutContent,
+        areaName: "chat",
+        maxHeight: "600px",
+        maxWidth: "300px",
+        marginY: "2.5rem",
+        title: "",
+        closeStrategy: "explicit",
+      },
+    });
+
+    this._flyoutSubscription = this._flyoutComponent.closed$.subscribe(() => {
+      this._logger.info("Destroyed flyout");
+      this._flyoutComponent?.$destroy();
+      this._flyoutSubscription?.unsubscribe();
+      this._flyoutComponent = undefined;
+    });
+  }
+
+  public get playerCandidatesStore() {
+    return fromObservable(this._playerCandidates$, this._playerCandidates$.value);
   }
 }
---
+++ src/app/features/ping-and-replies/suggestion-popover.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { PingAndRepliesFeature } from "@/app/features/ping-and-replies/ping-and-replies.feature";
+
+  export let feature: PingAndRepliesFeature;
+  export let onSelected: (name: string) => void;
+
+  const people = feature.playerCandidatesStore;
+</script>
+
+<div class="typo-ping-suggestion-popover">
+  {#if $people.length == 0}
+    No people
+  {:else}
+    {#each $people as person}
+      <button type="button">{person}</button>
+    {/each}
+
+    <button type="button" on:click={() => onSelected("Hy")}>send random stuff</button>
+  {/if}
+</div>
