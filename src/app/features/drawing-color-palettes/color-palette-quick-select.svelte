e251799e255fa3c1eb26da2bc923388f543c940b
tobeh <66030584+toobeeh@users.noreply.github.com>
2025-09-24 18:24:51 +0200

    add palette switcher shortcut toobeeh/skribbltypo#382

---
+++ src/app/features/drawing-color-palettes/color-palette-quick-select.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let palettes: string[];
+  export let onSubmit: (palette: string) => void;
+</script>
+
+<style lang="scss">
+
+  .typo-palette-switcher {
+    display: flex;
+    flex-direction: column;
+    gap: .5rem;
+
+    .typo-palette-switcher-item {
+      background-color: var(--COLOR_PANEL_BG);
+      border-radius: 5px;
+      cursor:pointer;
+      padding: .5em;
+    }
+  }
+
+</style>
+<span>Select a custom palette for drawing.<br>Please note that only typo players can see custom colors!</span>
+<br>
+
+<div class="typo-palette-switcher">
+  {#each palettes as palette}
+    <span class="typo-palette-switcher-item" on:click={() => onSubmit(palette)}>{palette}</span>
+  {/each}
+</div>
+
