969df022575f881d8ee2ebc411a65bac71358244
afx8732 <arcticfox8732@gmail.com>
2025-09-26 21:12:27 -0500

    [vip players] styling

--- src/app/features/chat-message-highlighting/vip-players-manage.svelte
+++ src/app/features/chat-message-highlighting/vip-players-manage.svelte
@@ -25,40 +25,61 @@
   };
 </script>
 
-<b>VIP Players</b>
-<div>Highlight your friends' messages in chat!</div>
-{#if $vipListStore.length == 0}
-  No VIPs added!
-{:else}
-  {#each $vipListStore as vip, index}
-    <div class="viplist-person">
-      <div>{vip.name}</div>
-      <div>
-        <input type="color" value={vip.color} on:change={(e) => updateColor(index, e)} />
-      </div>
-      <div>
-        <FlatButton on:click={() => removePlayer(index)} content="Remove" color="orange" />
-      </div>
+<div class="viplist-mgr">
+  <b>VIP Players</b>
+  <div>Highlight your friends' messages in chat!</div>
+  {#if $vipListStore.length == 0}
+    No VIPs added!
+  {:else}
+    <div class="viplist-list">
+      {#each $vipListStore as vip, index}
+        <div class="viplist-person">
+          <div>{vip.name}</div>
+          <div>
+            <input type="color" value={vip.color} on:change={(e) => updateColor(index, e)} />
+          </div>
+          <div>
+            <FlatButton on:click={() => removePlayer(index)} content="Remove" color="orange" />
+          </div>
+        </div>
+      {/each}
     </div>
-  {/each}
-{/if}
-<div class="viplist-add">
-  <div>
-    <input type="text" placeholder="Name..." bind:value={addInputTxt} />
-    <input type="color" bind:value={addInputCol} />
+  {/if}
+  <div class="viplist-add">
+    <div>
+      <input type="text" placeholder="Name..." bind:value={addInputTxt} />
+      <input type="color" bind:value={addInputCol} />
+    </div>
+    <FlatButton content="Add" on:click={() => addPlayer()} color="blue" />
   </div>
-  <FlatButton content="Add" on:click={() => addPlayer()} color="blue" />
 </div>
 
 <style lang="scss">
+  .viplist-mgr {
+    max-width: 500px;
+    background-color: var(--COLOR_PANEL_HI);
+    border-radius: 3px;
+    padding: 10px;
+  }
+
+  .viplist-list {
+    background-color: #fff4;
+    border-radius: 5px;
+    padding: 8px;
+    padding-right: 16px;
+    margin: 8px 0;
+  }
+
   .viplist-person {
     display: flex;
     flex-direction: row;
     gap: 5px;
     align-items: center;
+    margin-bottom: 3px;
 
     > div {
       flex-grow: 1;
+
       &:not(:first-child) {
         max-width: 70px;
       }
@@ -68,6 +89,7 @@
   .viplist-add {
     display: flex;
     flex-direction: column;
+    margin-top: 2px;
 
     > div {
       display: flex;
@@ -76,7 +98,7 @@
       margin-bottom: 5px;
 
       > input {
-        width: auto;
+        width: 0;
 
         &:first-child {
           flex-grow: 1;
